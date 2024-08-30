e62b0d7a11736b5e142d19bd67f37501b60b4b5f
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-08-30 19:17:10 +0300

    added catalog page

--- src/pages/catalog_page/catalog_page.jsx
+++ src/pages/catalog_page/catalog_page.jsx
@@ -1,10 +1,31 @@
-import BottomMenu from "../../components/bottom_menu/bottom_menu"
+import BottomMenu from "../../components/bottom_menu/bottom_menu";
+import styles from "./catalog_page.module.css";
+
+import arrowBackIcon from "../../images/arrow_back_icon.svg";
+import searchIcon from "../../images/search_icon.svg";
+import sortIcon from "../../images/sort_icon.svg";
+import filterIcon from "../../images/filter_icon.svg";
 
 const CatalogPage = () => {
-    return <div>
-        <p style={{color: "white"}}>Каталог</p>
-        <BottomMenu/>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.backButton}>
+          <img src={arrowBackIcon} alt="" />
+        </div>
+        <form className={styles.searchField}>
+          <input type="search" placeholder="Найти" />
+        </form>
+        <div className={styles.sortButton}>
+          <img src={sortIcon} alt="" />
+        </div>
+        <div className={styles.filterButton}>
+          <img src={filterIcon} alt="" />
+        </div>
+      </div>
+      <BottomMenu />
     </div>
-}
+  );
+};
 
-export default CatalogPage
+export default CatalogPage;
