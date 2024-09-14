d3b2af972b275c61476f13b6b15774a5fc7c929c
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-15 00:31:17 +0300

    added adaptive catalog page

--- src/pages/catalog_page/catalog_page.jsx
+++ src/pages/catalog_page/catalog_page.jsx
@@ -6,10 +6,28 @@ import styles from "./catalog_page.module.css";
 import arrowBackIcon from "../../images/arrow_back_icon.svg";
 import sortIcon from "../../images/sort_icon.svg";
 import filterIcon from "../../images/filter_icon.svg";
+import useWindowDimensions from "../../components/hooks/windowDimensions";
 
 const CatalogPage = () => {
+  const { width } = useWindowDimensions();
   return (
-    <div className={styles.container}>
+    <div
+      className={
+        width >= 585
+          ? styles.container585_600
+          : width >= 565
+          ? styles.container565_585
+          : width >= 525
+          ? styles.container525_565
+          : width >= 485
+          ? styles.container485_525
+          : width >= 450
+          ? styles.container450_485
+          : width >= 410
+          ? styles.container410_450
+          : styles.container375_410
+      }
+    >
       <div className={styles.header}>
         <div className={styles.backButton}>
           <img src={arrowBackIcon} alt="" />
@@ -37,7 +55,6 @@ const CatalogPage = () => {
         </div>
         <div className={styles.productRow}>
           <ProductCard model_name=" Nike Air Jordan 5 Retro “Jade Horizon”" />
-          {/* <ProductCard /> */}
         </div>
       </div>
       <BottomMenu />
