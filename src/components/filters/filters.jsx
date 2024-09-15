c389df74f6fc6315b5d6c80be1445c28350169e8
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-15 16:35:10 +0300

    fixed filter page

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -3,9 +3,20 @@ import styles from "./filters.module.css";
 
 import divLine from "../../images/div_line.svg";
 import selectArrow from "../../images/arrow_select_white.svg";
+import { useState } from "react";
 
 const Filters = () => {
   const { width } = useWindowDimensions();
+  const [isVisible, setIsVisible] = useState([
+    [false, false, false, false, false, false],
+    [false, false],
+    [false, false],
+    [false, false],
+    [false, false],
+    [false, false],
+    [false, false],
+  ]);
+  let copyIsVisible = Array.from(isVisible);
   return (
     <div
       className={
@@ -41,33 +52,196 @@ const Filters = () => {
       <div className={styles.mainFilters}>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Бренд</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][0] = !copyIsVisible[0][0];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Бренд</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][0] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+
+          <div
+            className={
+              isVisible[0][0] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div
+              className={styles.subFilterRow}
+              onClick={() => {
+                copyIsVisible[1][0] = !copyIsVisible[1][0];
+                setIsVisible(copyIsVisible);
+              }}
+            >
+              <div className={`${styles.checkButton} `} />
+              <p className={styles.checkButtonText}>Adidas</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Nike</p>
+            </div>
+          </div>
         </div>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Цвет</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][1] = !copyIsVisible[0][1];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Цвет</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][1] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+          <div
+            className={
+              isVisible[0][1] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Белый</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Черный</p>
+            </div>
+          </div>
         </div>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Размер</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][2] = !copyIsVisible[0][2];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Размер</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][2] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+          <div
+            className={
+              isVisible[0][2] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>42</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>43</p>
+            </div>
+          </div>
         </div>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Сезон</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][3] = !copyIsVisible[0][3];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Сезон</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][3] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+          <div
+            className={
+              isVisible[0][3] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Лето</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Зима</p>
+            </div>
+          </div>
         </div>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Пол</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][4] = !copyIsVisible[0][4];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Пол</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][4] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+          <div
+            className={
+              isVisible[0][4] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Мужское</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Женское</p>
+            </div>
+          </div>
         </div>
         <div className={styles.divideLine} />
         <div className={styles.filterRow}>
-          <p className={styles.filterNameText}>Материал</p>
-          <img src={selectArrow} alt="" />
+          <div
+            className={styles.filterButton}
+            onClick={() => {
+              copyIsVisible[0][5] = !copyIsVisible[0][5];
+              setIsVisible(copyIsVisible);
+            }}
+          >
+            <p className={styles.filterNameText}>Материал</p>
+            <img
+              src={selectArrow}
+              alt=""
+              className={isVisible[0][5] ? styles.arrowClose : styles.arrowOpen}
+            />
+          </div>
+          <div
+            className={
+              isVisible[0][5] ? styles.subFiltersOpen : styles.subFiltersClose
+            }
+          >
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Полиэстер</p>
+            </div>
+            <div className={styles.subFilterRow}>
+              <div className={styles.checkButton} />
+              <p className={styles.checkButtonText}>Хлопок</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
