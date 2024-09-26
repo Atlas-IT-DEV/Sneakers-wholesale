9902758ffbfd1f39b90a34d6191b41c7f34a3b08
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-26 11:46:38 +0300

    added buttons

--- src/pages/checkout_page/checkout_page.jsx
+++ src/pages/checkout_page/checkout_page.jsx
@@ -3,10 +3,11 @@ import arrowBackIcon from "../../images/arrow_back_icon.svg";
 import sdekIcon from "../../images/sdek_icon.svg";
 import plusIcon from "../../images/plus_orange_icon.svg";
 import selectArrow from "../../images/arrow_light_gray.svg";
+import mirIcon from "../../images/mir_icon.svg";
 
 import { useNavigate } from "react-router";
 
-const CheckoutPage = () => {
+const CheckoutPage = ({ count = 2 }) => {
   const navigate = useNavigate();
   return (
     <div className={styles.container}>
@@ -16,16 +17,16 @@ const CheckoutPage = () => {
         </div>
         <p className={styles.namePageText}>Оформление заказа</p>
       </div>
-      <div className={styles.delivery}>
-        <p style={{ color: "white" }}>Доставка</p>
-        <div className={styles.pickUpPoint}>
-          <div className={styles.deliveryPoint}>
+      <div className={styles.view}>
+        <p className={styles.headerView}>Доставка</p>
+        <div className={styles.selectButton}>
+          <div className={styles.contentView}>
             <img src={sdekIcon} alt="" />
             <p>Выбрать пункт выдачи</p>
           </div>
           <img src={plusIcon} />
         </div>
-        <div className={styles.shippingMethodButton}>
+        <div className={styles.methodButton}>
           <p>Все способы доставки</p>
           <img src={selectArrow} alt="" />
         </div>
@@ -38,7 +39,56 @@ const CheckoutPage = () => {
         <img src="" alt="" />
         <img src="" alt="" />
       </div>
-      <div></div>
+      <div className={`${styles.view} ${styles.payment}`}>
+        <p className={styles.headerView}>Способ оплаты</p>
+        <div className={styles.selectButton}>
+          <div className={styles.contentView}>
+            <img src={mirIcon} alt="" />
+            <p>Привязать карту</p>
+          </div>
+          <img src={plusIcon} />
+        </div>
+        <div className={styles.methodButton}>
+          <p>Все способы оплаты</p>
+          <img src={selectArrow} alt="" />
+        </div>
+      </div>
+      <div className={styles.divLine} />
+      <div className={styles.totalView}>
+        <p className={`${styles.attributeTotal} ${styles.totalText}`}>Итого</p>
+        <p className={`${styles.valueTotal} ${styles.totalText}`}>18400₽</p>
+      </div>
+      <div className={styles.descriptionView}>
+        <div className={styles.viewTotal}>
+          <p className={`${styles.descAttrTotal} ${styles.descText}`}>
+            {count} товара на сумму
+          </p>
+          <p className={`${styles.descValueTotal} ${styles.descText}`}>
+            18400₽
+          </p>
+        </div>
+        <div className={styles.viewTotal}>
+          <p className={`${styles.descAttrTotal} ${styles.descText}`}>Скидка</p>
+          <p className={`${styles.descValueTotal} ${styles.descText}`}>
+            -18400₽
+          </p>
+        </div>
+        <div className={styles.viewTotal}>
+          <p className={`${styles.descAttrTotal} ${styles.descText}`}>
+            Доставка
+          </p>
+          <p className={`${styles.descValueTotal} ${styles.descText}`}>
+            Бесплатно
+          </p>
+        </div>
+      </div>
+      <div className={styles.orderButton}>
+        <p>Заказать</p>
+        <div>
+          <p>18500₽</p>
+          <p>18500₽</p>
+        </div>
+      </div>
     </div>
   );
 };
